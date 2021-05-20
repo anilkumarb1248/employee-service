9e37777812719026a0e6e8cf9920c445c3b9379e
anilkumarb1248 <anilkumarb1248@gmail.com>
2021-05-21 00:29:34 +0530

    Cache Integration (EhCache,Redis and HazelCast) with spring boot and finally HazelCast cache present all others are commented

---
+++ src/main/java/com/app/bak/config/cache/EHCacheV2Config.java
@@ -0,0 +1,30 @@
+package com.app.bak.config.cache;
+
+//import org.springframework.cache.CacheManager;
+//import org.springframework.cache.ehcache.EhCacheCacheManager;
+//import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+
+/**
+ * This is EhCache 2.x version configuration. if you want to work with ehcache 2.x, uncomment this
+ * and also you need to rename the ehcacheV2.xml to ehcache.xml file.
+ *
+ */
+//@Configuration
+public class EHCacheV2Config {
+
+//	@Bean
+//	CacheManager cacheManager() {
+//		return new EhCacheCacheManager(ehCacheManager());
+//	}
+//
+//	private net.sf.ehcache.CacheManager ehCacheManager() {
+//		EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+//		factoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+//		factoryBean.setShared(true);
+//		return factoryBean.getObject();
+//	}
+
+}
---
+++ src/main/java/com/app/bak/config/cache/EhCacheV3EventLogger.java
@@ -0,0 +1,18 @@
+package com.app.bak.config.cache;
+
+//import org.ehcache.event.CacheEvent;
+//import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EhCacheV3EventLogger{
+//implements CacheEventListener<Object, Object> {
+//	Logger LOGGER = LoggerFactory.getLogger(EhCacheV3EventLogger.class);
+//
+//	@Override
+//	public void onEvent(CacheEvent<? extends Object, ? extends Object> cacheEvent) {
+//		LOGGER.info("message ----- ", cacheEvent.getKey(), cacheEvent.getOldValue(), cacheEvent.getNewValue());
+//
+//	}
+
+}
